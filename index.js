f5f1329925a78736fd45e167d9111b99d45d9c6d
Hrishikesh Mallick <rishikeshcrever@gmail.com>
2020-11-09 19:49:47 +0530

    Layout added

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const port=8000;
 const server=express();
 server.set('view engine','ejs');
 server.set('views',__dirname+'/views');
+const expressLayouts= require('express-ejs-layouts');
+server.use(expressLayouts);           //layout format
+server.set('layout extractScripts',true);     // script tags for layout
+server.set('layout extractStyles',true);      // css tags for layout
+server.set('layout','layouts/layout.ejs');
 const db=require('./config/config');   //connecting to database
 server.use(express.urlencoded());      //middleware for parsing the incoming data
 server.use(express.static(__dirname+'/assets'));  //routing static files
